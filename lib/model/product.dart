cf7059d7fdd5e7018801ff8e466161ac6a7bea15
otabekinha <otabekinha@gmail.com>
2024-10-28 14:40:11 +0500

    Added Dummy Data

---
+++ lib/model/product.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String id;
+  final String title;
+  final String desc;
+  final String image;
+  final double price;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.desc,
+    required this.image,
+    required this.price,
+  });
+}
